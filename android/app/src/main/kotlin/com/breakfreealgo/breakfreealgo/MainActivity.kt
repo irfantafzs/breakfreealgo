f242214d1d6b54857a174f4f0b8614072f0d2769
irfantafzs <irfan@tafzs.com>
2020-11-14 14:38:46 +0530

    login page
    initial version

---
+++ android/app/src/main/kotlin/com/breakfreealgo/breakfreealgo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.breakfreealgo.breakfreealgo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/welcomepage.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:breakfreealgo/src/loginpage.dart';
+import 'package:breakfreealgo/src/signup.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+
+  Widget _submitButton() {
+    // SizeConfig().init(context);
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        // width: SizeConfig.blockSizeHorizontal*50,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color(0xff2cd1d1).withAlpha(100), //0xffdf8e33
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Colors.white),
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 20, color: Color(0xff4fae8d)),
+        ),//0xfff7892b
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    // SizeConfig().init(context);
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SignUpPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        // width: SizeConfig.blockSizeHorizontal*50,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child: Text(
+          'Open a new account',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _label() {
+    return Container(
+        margin: EdgeInsets.only(top: 40, bottom: 20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Quick login with Touch ID',
+              style: TextStyle(color: Colors.white, fontSize: 17),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Icon(Icons.fingerprint, size: 90, color: Colors.white),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Touch ID',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Widget _icon() {
+    return Image(image: AssetImage('images/icon.png'));
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'Breakfree',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.bodyText1,
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+          children: [
+            TextSpan(
+              text: 'algo',
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+          ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SingleChildScrollView(
+        child:Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.grey.shade200,
+                      offset: Offset(2, 4),
+                      blurRadius: 5,
+                      spreadRadius: 2)
+                ],
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xff4fae8d), Color(0xff2cd1d1)])), //0xfffbb448, 0xffe46b10
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _icon(),
+                _title(),
+                SizedBox(
+                  height: 80,
+                ),
+                _submitButton(),
+                SizedBox(
+                  height: 20,
+                ),
+                _signUpButton(),
+                SizedBox(
+                  height: 20,
+                ),
+                _label()
+              ],
+            ),
+          ),
+      ),
+    );
+  }
+}
